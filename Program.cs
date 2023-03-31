c08b65d3ad89d197b4c86da0af488b6040a8974e
sinan <sinan_mkn_07@hotmail.com>
2023-03-31 19:15:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Merhaba adinizi giriniz:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Adiniz:",name);
+    }
+}
+}
